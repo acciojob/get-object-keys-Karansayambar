55d2d95e612203ee7be42b18c02421251755272c
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-14 23:00:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const student = {
+  name: "John Doe"
+};
+
+// Add getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  // Get all keys of the object and return them as an array
+  return Object.keys(this);
+};
+
+// Test the getKeys() method
+const keysArray = student.getKeys();
+console.log(keysArray);
